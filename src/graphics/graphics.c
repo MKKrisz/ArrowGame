290b27225175e2d1e6ec8a80a0f01f3e1ddabe21
MKKrisz <magyarikrisztianbusiness@gmail.com>
2023-10-30 16:30:08 +0100

    Added error-reporting to SDL calls in graphics.c

--- src/graphics/graphics.c
+++ src/graphics/graphics.c
@@ -4,6 +4,18 @@
 #include <SDL2/SDL_image.h>
 #include "../../debugmalloc.h"
 
+#define PrintError(x) if(x != 0){ \
+    printf("%s at %s:%d\n", SDL_GetError(), __FILE__, __LINE__); \
+}
+#define PrintErrorPointer(x) if(x == NULL){ \
+    printf("%s at %s:%d\n", SDL_GetError(), __FILE__, __LINE__); \
+}
+#define PrintErrorImg(x) if(x == NULL){ \
+    printf("%s at %s:%d\n", IMG_GetError(), __FILE__, __LINE__); \
+}
+
+
+
 
 /* File Format: WIDTH HEIGHT SCALING FULLSCREEN
  * I differ from the original spec here, as instead of storing a single bool in the file for
@@ -29,16 +41,12 @@ void gfx_Save(gfxcfg* config, char* path){
 
 gfxcfg gfx_Default(){
     SDL_DisplayMode current;
-    if(SDL_GetCurrentDisplayMode(0, &current) != 0){
-        printf("%s\n", SDL_GetError());
-    }
+    PrintError(SDL_GetCurrentDisplayMode(0, &current));
     return (gfxcfg){current.w, current.h, BASE_SCALING, SDL_WINDOW_SHOWN | SDL_WINDOW_FULLSCREEN};
 }
 
 void Init_Gfx(){
-    if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
-        printf("%s", SDL_GetError());
-    }
+    PrintError(SDL_Init(SDL_INIT_EVERYTHING));
 }
 
 
@@ -49,15 +57,18 @@ Graphics CreateGraphics(gfxcfg config){
 Graphics CreateGraphicsRaw(const char* wName, int width, int height, float scaling, uint flags){
     Graphics g;
     g.Window = SDL_CreateWindow(wName, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, flags);
+    PrintErrorPointer(g.Window);
     if(flags & SDL_WINDOW_FULLSCREEN){
         SDL_DisplayMode dm;
-        SDL_GetCurrentDisplayMode(0, &dm);
+        PrintError(SDL_GetCurrentDisplayMode(0, &dm));
         dm.w = width; dm.h = height;
-        SDL_SetWindowDisplayMode(g.Window, &dm);
+        PrintError(SDL_SetWindowDisplayMode(g.Window, &dm));
     }
     g.Renderer = SDL_CreateRenderer(g.Window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE | SDL_RENDERER_PRESENTVSYNC);
- 
+    PrintErrorPointer(g.Renderer);
+
     g.Arrow = IMG_LoadTexture(g.Renderer, "Assets/Arrow.png");
+    PrintErrorImg(g.Arrow);
 
     g.window_width = width;
     g.window_height = height;
@@ -66,6 +77,7 @@ Graphics CreateGraphicsRaw(const char* wName, int width, int height, float scali
     g.viewport_width = (int)(width * scaling);
     g.viewport_height = (int)(height * scaling);
     g.Viewport = SDL_CreateTexture(g.Renderer, SDL_PIXELFORMAT_ARGB32, SDL_TEXTUREACCESS_TARGET, g.viewport_width, g.viewport_height);
+    PrintErrorPointer(g.Viewport);
     return g;
 }
 
@@ -84,16 +96,17 @@ void RescaleViewport(Graphics* g, float scale){
 
     SDL_DestroyTexture(g->Viewport);
     g->Viewport = SDL_CreateTexture(g->Renderer, SDL_PIXELFORMAT_ARGB32, SDL_TEXTUREACCESS_STREAMING, g->viewport_width, g->viewport_height);
+    PrintErrorPointer(g->Viewport);
 }
 
 void BeginDraw(Graphics* g){
-    SDL_SetRenderTarget(g->Renderer, g->Viewport);
-    SDL_SetRenderDrawColor(g->Renderer, 0, 0, 0, 0);
-    SDL_RenderClear(g->Renderer);
+    PrintError(SDL_SetRenderTarget(g->Renderer, g->Viewport));
+    PrintError(SDL_SetRenderDrawColor(g->Renderer, 0, 0, 0, 0));
+    PrintError(SDL_RenderClear(g->Renderer));
 }
 
 void EndDraw(Graphics* g){
-    SDL_SetRenderTarget(g->Renderer, NULL);
-    SDL_RenderCopyEx(g->Renderer, g->Viewport, NULL, NULL, 0, NULL, SDL_FLIP_VERTICAL);
+    PrintError(SDL_SetRenderTarget(g->Renderer, NULL));
+    PrintError(SDL_RenderCopyEx(g->Renderer, g->Viewport, NULL, NULL, 0, NULL, SDL_FLIP_VERTICAL));
     SDL_RenderPresent(g->Renderer);
 }
